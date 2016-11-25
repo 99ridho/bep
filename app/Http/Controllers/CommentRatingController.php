2c98fe0bf7991fed086aece1c5d72cd5479df2d3
Muhammad Ridho K. Pratama <99ridho@gmail.com>
2016-11-26 02:21:36 +0700

    template code rating, comment, schedule

---
+++ app/Http/Controllers/CommentRatingController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class CommentRatingController extends Controller
+{
+
+    // TODO: Comment rating!
+
+    public function addRating(Request $request) {
+        //
+    }
+
+    public function addComment(Request $request) {
+        //
+    }
+}
---
+++ app/Http/Controllers/ScheduleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ScheduleController extends Controller
+{
+
+    // TODO : jadwal pertandingan yang diikuti...
+
+    public function getSchedule($id) {
+
+    }
+}
